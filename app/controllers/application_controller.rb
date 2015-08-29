462093a39cd2186cc02d93fb93ff346cd507a20e
Dean Solecki <deansolecki@gmail.com>
2015-08-28 19:55:51 -0700

    Changed config in app_ctrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::API
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:profile_attributes => [:nickname, :first_name, :last_name])}
+		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:profile)}
 	end
 
 	private
