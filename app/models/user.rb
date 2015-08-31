5ba2ed2c33066d1753b556c9d59048454410dbc2
Dean Solecki <deansolecki@gmail.com>
2015-08-31 11:02:38 -0700

    deleted overrides dir and removed :profile from ctrl

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,4 @@ class User < ActiveRecord::Base
 	has_one :profile, :as => :profileable, dependent: :destroy, autosave: true
 	accepts_nested_attributes_for :profile
 	
-	module DeviseTokenAuth::Concerns::User
-		extend ActiveSupport::Concern
-
-		def token_validation_response
-			UserSerializer.new(self, root: false)
-
-			all the shit should get fucked here
-		end
-	end
 end
