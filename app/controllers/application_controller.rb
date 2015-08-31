80aecb7f90bc0c80776a1c47423cc3b274567513
Dean Solecki <deansolecki@gmail.com>
2015-08-31 09:51:43 -0700

    forked devise_token_auth and configured accordingly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,9 @@
 class ApplicationController < ActionController::API
 	before_action :set_default_response_format
-	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	include DeviseTokenAuth::Concerns::SetUserByToken
-	include ActionController::MimeResponds
 	include ActionController::ImplicitRender
 	include ActionController::Serialization
-
-	protected
-
-	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:profile_attributes => [:nickname, :first_name, :last_name])}
-	end
 
 	private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 	has_many :comments, :as => :commentable
-	has_one :profile, :as => :profileable, dependent: :destroy
-	validates :profile, presence: true
+	has_one :profile, :as => :profileable, dependent: :destroy, autosave: true
+	accepts_nested_attributes_for :profile
+	
+	module DeviseTokenAuth::Concerns::User
+		extend ActiveSupport::Concern
+
+		def token_validation_response
+			UserSerializer.new(self, root: false)
+
+			all the shit should get fucked here
+		end
+	end
 end
